f311237e5071c196f16e39d1932a26b6a76e1157
James-hero <155511065+James-hero@users.noreply.github.com>
2024-01-04 20:41:50 +0900

    Update Slacker_notices.py

--- slack_notice/src/Slacker_notices.py
+++ slack_notice/src/Slacker_notices.py
@@ -21,34 +21,42 @@ weekdays_dict = {
     6 : '일'
 }
 
-weekdays = datetime.weekday(datetime.today())
-
-if weekdays == 5 or weekdays == 6:
-    print(f'weekdays : {weekdays}')
-else:
-    str1_title = 'KOSPI, ' + today_date + ' (' + weekdays_dict[weekdays] + ')'
-
-    buySell = '개인 : ' + trend[0] + ', 외국인 : ' + trend[1] + ', 기관 : ' + trend[2] + '\n'
-    programBuySell = '차익 : ' + trend[3] + ', 비차익 : ' + trend[4] + ', 총합 : ' + trend[5] + '\n'
-    kospi_value = today_value[0] + today_value[1] + '\n'
-    total_text = kospi_value + buySell + programBuySell
-
-    if today_value[1].find('+') != -1: # + 인 경우,
-        today_color = '#ff0000' # 빨강색
+def job():
+    weekdays = datetime.weekday(datetime.today())
+    
+    if weekdays == 5 or weekdays == 6:
+        print(f'weekdays : {weekdays}')
     else:
-        today_color = '#0000ff' # 파랑색
+        str1_title = 'KOSPI, ' + today_date + ' (' + weekdays_dict[weekdays] + ')'
+    
+        buySell = '개인 : ' + trend[0] + ', 외국인 : ' + trend[1] + ', 기관 : ' + trend[2] + '\n'
+        programBuySell = '차익 : ' + trend[3] + ', 비차익 : ' + trend[4] + ', 총합 : ' + trend[5] + '\n'
+        kospi_value = today_value[0] + today_value[1] + '\n'
+        total_text = kospi_value + buySell + programBuySell
+    
+        if today_value[1].find('+') != -1: # + 인 경우,
+            today_color = '#ff0000' # 빨강색
+        else:
+            today_color = '#0000ff' # 파랑색
+    
+    
+        attach_dict = {
+            'color' : today_color,
+            'author_name' : '오늘의 증시 알림',
+            'title' : str1_title,
+            'title_link' : 'http://finance.naver.com/sise/sise_index.nhn?code=KOSPI',
+            'text' : total_text
+        }
+        
+        attach_list=[attach_dict]
+        
+        notice_message(Token, "#stock_notice", str1_title, attach_list)
+        upload_file(Token, "#stock_notice", upload_image, today_date + '_KOSPI.png')
 
+schedule.every().day.at("07:00").do(job)
+# schedule.every(3).seconds.do(job) # 3초마다 job 실행
 
-    attach_dict = {
-        'color' : today_color,
-        'author_name' : '오늘의 증시 알림',
-        'title' : str1_title,
-        'title_link' : 'http://finance.naver.com/sise/sise_index.nhn?code=KOSPI',
-        'text' : total_text
-    }
-    
-    attach_list=[attach_dict]
-    
-    notice_message(Token, "#stock_notice", str1_title, attach_list)
-    upload_file(Token, "#stock_notice", upload_image, today_date + '_KOSPI.png')
-    
+while True:
+    schedule.run_pending()
+    time.sleep(1)    
+
